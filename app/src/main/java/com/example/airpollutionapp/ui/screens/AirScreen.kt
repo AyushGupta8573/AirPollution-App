80b88482974599ce928ed66436ace7b6720a1a8c
AyushGupta8573 <ayushgupta160803@gmail.com>
2024-08-26 23:14:07 +0530

    My aappp

--- app/src/main/java/com/example/airpollutionapp/ui/screens/AirScreen.kt
+++ app/src/main/java/com/example/airpollutionapp/ui/screens/AirScreen.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,10 +36,10 @@ import com.example.airpollutionapp.network.Components
 
 @Composable
 fun AirScreen(
-    modifier: Modifier= Modifier,
+    modifier: Modifier = Modifier,
     state: WeatherState
 ) {
-    var isOpen :Boolean by remember{ mutableStateOf(false) }
+    var isOpen: Boolean by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -46,13 +50,17 @@ fun AirScreen(
     ) {
         Card(
             modifier = modifier
-
+                .padding(8.dp)
+                .fillMaxWidth()
         ) {
             Text(
                 text = "Air Pollution Application",
-                fontSize = 10.sp,
+                fontSize = 20.sp,
                 fontFamily = FontFamily.Serif,
-                fontWeight = FontWeight.ExtraBold
+                color = Color.Blue,
+                fontWeight = FontWeight.ExtraBold,
+                modifier = modifier
+                    .align(alignment = Alignment.CenterHorizontally)
             )
         }
 
@@ -60,8 +68,16 @@ fun AirScreen(
 
 
 
-        Button(onClick = {isOpen = !isOpen}) {
-            Text(text = "Click Here")
+        Button(
+            onClick = { isOpen = !isOpen },
+            modifier = Modifier,
+            colors = ButtonDefaults.buttonColors(Color.Black)
+        ) {
+            Text(
+                text = "Click Here",
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.ExtraBold,
+            )
         }
         if (isOpen) {
             Column {
@@ -71,7 +87,7 @@ fun AirScreen(
     }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 private fun AirScreenPreview() {
     AirScreen(state = WeatherState())
